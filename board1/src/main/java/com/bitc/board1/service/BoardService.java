f0835fd0a88309c594b53728539687c026f4a23b
lecture-bisper <bisper@outlook.kr>
2023-11-24 16:30:53 +0900

    11/23 - board1 프로젝트의 Service 인터페이스와 구현 클래스 추가, 게시글 추가하기, 게시글 상세보기, 게시글 조회수 증가하기, 게시글 수정하기, 게시글 삭제하기.

--- board1/src/main/java/com/bitc/board1/service/BoardService.java
+++ board1/src/main/java/com/bitc/board1/service/BoardService.java
@@ -10,4 +10,21 @@ import java.util.List;
 public interface BoardService {
 
   public List<BoardDto> selectBoardList() throws Exception;
+
+  public void insertBoard(BoardDto board) throws Exception;
+
+  public BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+  public void updateBoard(BoardDto board) throws Exception;
+
+  public void deleteBoard(int boardIdx) throws Exception;
 }
+
+
+
+
+
+
+
+
+
--- board1/src/main/java/com/bitc/board1/service/BoardServiceImpl.java
+++ board1/src/main/java/com/bitc/board1/service/BoardServiceImpl.java
@@ -21,6 +21,37 @@ public class BoardServiceImpl implements BoardService {
   public List<BoardDto> selectBoardList() throws Exception {
     return boardMapper.selectBoardList();
   }
+
+//  글 등록
+  @Override
+  public void insertBoard(BoardDto board) throws Exception {
+//  mapper을 사용하여 데이터 베이스에 글 등록
+    boardMapper.insertBoard(board);
+  }
+
+//  상세 글 보기
+  @Override
+  public BoardDto selectBoardDetail(int boardIdx) throws Exception {
+//    mapper을 사용하여 해당 글번호의 조회수 증가
+    boardMapper.updateHitCount(boardIdx);
+//    mapper을 사용하여 데이터 베이스에서 지정한 글의 상세 내용 가져오기
+    BoardDto board = boardMapper.selectBoardDetail(boardIdx);
+    return board;
+  }
+
+//  게시글 수정
+  @Override
+  public void updateBoard(BoardDto board) throws Exception {
+//    mapper를 사용하여 데이터 베이스의 내용을 사용자가 입력한 데이터로 수정
+    boardMapper.updateBoard(board);
+  }
+
+//  게시글 삭제
+  @Override
+  public void deleteBoard(int boardIdx) throws Exception {
+//    mapper를 사용하여 데이터 베이스의 내용을 삭제
+    boardMapper.deleteBoard(boardIdx);
+  }
 }
 
 
