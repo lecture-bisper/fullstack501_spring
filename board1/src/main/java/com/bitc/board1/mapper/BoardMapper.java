e96112020bb2d82dfc74ba1f720e7da3287be34f
lecture-bisper <bisper@outlook.kr>
2023-11-24 16:29:45 +0900

    11/23 - board1 프로젝트의 mapper 기능 추가, 게시글 등록하기, 게시글 상세보기, 게시글 수정하기, 게시글 삭제하기, 게시글 조회수 증가하기.

--- board1/src/main/java/com/bitc/board1/mapper/BoardMapper.java
+++ board1/src/main/java/com/bitc/board1/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package com.bitc.board1.mapper;
 
 import com.bitc.board1.dto.BoardDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -13,4 +14,27 @@ import java.util.List;
 public interface BoardMapper {
 
   public List<BoardDto> selectBoardList() throws Exception;
+
+//  연동된 xml 파일의 SQL문을 실행하여 데이터 베이스에 글 등록
+  public void insertBoard(BoardDto board) throws Exception;
+
+//  @Param : mybatis의 xml에서 사용할 매개변수의 이름을 직접 지정할 수 있는 어노테이션
+//  자바 기본 타입을 사용했을 경우 사용할 수 있으며, 해당 어노테이션을 사용하지 않을 경우 매개변수 이름을 xml에서 그대로 사용할 수 있음
+  public BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+  public void updateHitCount(@Param("boardIdx") int idx) throws Exception;
+
+  public void updateBoard(BoardDto board) throws Exception;
+
+  public void deleteBoard(@Param("boardIdx") int boardIdx) throws Exception;
 }
+
+
+
+
+
+
+
+
+
+
