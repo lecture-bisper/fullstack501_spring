a6af9b2e14619778ada522d774e4e47b9923bfd9
lecture-bisper <bisper@outlook.kr>
2023-11-30 09:08:27 +0900

    11/29 board2 프로젝트, 로그인 구현을 위한 DTO 클래스 구현.

---
+++ board2/src/main/java/com/bitc/board2/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.bitc.board2.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+  private String userId;
+  private String userPw;
+  private String userName;
+  private String userEmail;
+  private String userCreateDate;
+  private String userUpdateDate;
+}
+
+
+
+
+
+
+
